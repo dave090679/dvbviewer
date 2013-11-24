74b7f0ba4c287c91933e7fb7c84ddc822007cb00
David Parduhn <xkill85@gmx.net>
2013-11-24 10:27:41 +0100

    fixed an issue which causes the events to get lost. Now the add-on is ready for further development:-).

--- addon/appmodules/dvbviewer.py
+++ addon/appmodules/dvbviewer.py
@@ -18,25 +18,26 @@ impPath = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(impPath)
 # dvbviewerhelper wurde von comtypes.client.getactiveobject automatisch erzeugt. Das Modul enthielt allerdings einen Fehler, der die in meiner Ausgangsnachricht gezeigte Meldung cannot assign to None erzeugt hat. Im Modul gab es auch tatsächlich eine Zeile none = 0, die ich auskommentiert habe und danach ließ sich das Modul auch problemlos importieren.
 import dvbviewerhelper
+from logHandler import log
 # hier sollen meine Ereignisbehandlungsroutinen rein
-class dvbv_eventsink:
+class dvbv_eventsink(object):
 	# die als Kommentar hinterlegten Anweisungen kann ich leider nicht testen, weil keine Ereignisse durchkommen.
 	def __init__(self, server):
 		self.server = server
 		super(dvbv_eventsink, self).__init__()
 	def IDVBViewerEvents_onAction(self,ActionID):
-		log.info("IDVBViewerEvents_onAction("+str(ActionID)+")")
+		log.debugWarning("IDVBViewerEvents_onAction("+str(ActionID)+")")
 	def IDVBViewerEvents_onControlChange(self,WindowID, ControlID):
 		# ui.message(self.server.datamanager.value('#currentcontrol'))
-		log.info("IDVBViewerEvents_onControlChange("+str(WindowID)+", "+str(ControlID))
+		log.debugWarning("IDVBViewerEvents_onControlChange("+str(WindowID)+", "+str(ControlID))
 	def IDVBViewerEvents_OnPlaystatechange(self,RendererType, State):
-		log.info("IDVBViewerEvents_OnPlaystatechange("+str(RendererType)+', '+str(State))
+		log.debugWarning("IDVBViewerEvents_OnPlaystatechange("+str(RendererType)+', '+str(State))
 
 	def IDVBViewerEvents_onSelectedItemChange(self):
 		# ui.message(self.server.datamanager.value('#selecteditem'))
-		log.info("IDVBViewerEvents_onSelectedItemChange()")
+		log.debugWarning("IDVBViewerEvents_onSelectedItemChange()")
 	def IDVBViewerEvents_onOSDWindow(self,WindowID):
-		log.info("IDVBViewerEvents_onOSDWindow("+str(ID)+")")
+		log.debugWarning("IDVBViewerEvents_onOSDWindow("+str(ID)+")")
 		# ui.message(self.server.datamanager.value('#currentwindow'))
 
 class AppModule(appModuleHandler.AppModule):
@@ -44,9 +45,9 @@ class AppModule(appModuleHandler.AppModule):
 		super(AppModule, self).__init__(*flags, **kwflags)
 		# der NVDA braucht eine kleine Denkpause, weil er sonst zu früh versuchen würde, das com-Objekt anzuzapfen. und dann schreibt er entweder "Vorgang nicht verfügbar" oder "der Aufruf wurde durch messagefilter abgebrochen" ins Protokoll.
 		time.sleep(10)
-		dvbviewer = comtypes.client.GetActiveObject(dvbviewerhelper.DVBViewer, interface=dvbviewerhelper.IDVBViewer)
+		self.dvbviewer = comtypes.client.GetActiveObject(dvbviewerhelper.DVBViewer, interface=dvbviewerhelper.IDVBViewer)
 		# Und hier vermute ich das Problem. Aus irgendeinem Grund kommen die Ereignisse nicht zu meiner Ereignissenke durch.
-		connection = comtypes.client.GetEvents(dvbviewer.Events,dvbv_eventsink,dvbviewerhelper.IDVBViewerEvents)
+		self.connection = comtypes.client.GetEvents(self.dvbviewer.Events,dvbv_eventsink(self.dvbviewer),dvbviewerhelper.IDVBViewerEvents)
 
 
 
